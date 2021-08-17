5d316bf555583961acf059eb9eed69345412a23a
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-08-17 17:48:59 -0300

    ➕  TypeORM config

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+
+  migrations: ["src/database/migrations/*.ts"],
+
+  entities: ["src/entities/*.ts"],
+  
+  cli: {
+    migrationsDir: "src/database/migrations",
+    entitiesDir: "src/entities"
+  },
+}
